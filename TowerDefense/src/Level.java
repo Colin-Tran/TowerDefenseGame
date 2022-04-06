a2144520a63c59fc92f24d14a85a5fb68f408ccf
1929021 <1929021@01-19976.pusd.dom>
2022-04-06 11:51:39 -0700

    Created the classes for level and player. It has the instance variables

---
+++ TowerDefense/src/Level.java
@@ -0,0 +1,10 @@
+
+public class Level {
+	private int mode;
+	private int level;
+	private int maxNumberOfEnemies;
+	private int duration;
+	
+	private int startTime;
+	private int enemyCounter;
+}
---
+++ TowerDefense/src/Player.java
@@ -0,0 +1,6 @@
+
+public class Player {
+	private int lives;
+	private int money;
+	private int score;
+}
