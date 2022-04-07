422632d2219bbbd4c65e6f35567865e34d9eafdf
1929021 <1929021@01-19976.pusd.dom>
2022-04-07 10:57:23 -0700

    added more methods to those classes

--- TowerDefense/src/Level.java
+++ TowerDefense/src/Level.java
@@ -7,4 +7,16 @@ public class Level {
 	
 	private int startTime;
 	private int enemyCounter;
+	public Level(int mode, int level) {
+		
+	}
+	public void startLevel() {
+		
+	}
+	public int remainingTime() {
+		return 0;
+	}
+	public SlimeEnemy spawnEnemy(int x, int y, Object enemy)  {
+		return null;
+	}
 }
--- TowerDefense/src/Player.java
+++ TowerDefense/src/Player.java
@@ -3,4 +3,27 @@ public class Player {
 	private int lives;
 	private int money;
 	private int score;
+	public Player() {
+		lives = 20;
+		money = 75;
+		score = 0;
+	}
+	public int getLives() {
+		return lives;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setLives(int amount) {
+		lives = amount;
+	}
+	public void setMoney(int amount) {
+		money = amount;
+	}
+	public void setScore(int amount) {
+		score = amount;
+	}
 }
