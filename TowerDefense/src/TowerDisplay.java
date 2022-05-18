d9e89d4f9399a44d5c3a5806ae936eaebf1bffe7
1929021 <1929021@01-19976.pusd.dom>
2022-05-18 11:09:09 -0700

    added the instructions of how to put down the squirt tower

--- TowerDefense/src/TowerDisplay.java
+++ TowerDefense/src/TowerDisplay.java
@@ -30,6 +30,8 @@ public class TowerDisplay {
 		g.drawString(tower1.toString2(), x+60, y+30);
 		g.drawString(tower2.toString1(), x+60, y+80);
 		g.drawString(tower2.toString2(), x+60, y+90);
+		g.drawString("press 1 + left click to place it down", x+60, y+45);
+		g.drawString("press 2 and click to put down", x+60, y+105);
 		
 		//Color color = new Color(255, 153, 51); //Sets text to orange, also used to see hit boxes. Is movable to different parts of code to hide/show hit boxes.
 		//g.setColor(color);
